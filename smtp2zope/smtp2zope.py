faa80717090a7e87be240225ccf2fdb696bd8415
Maurits van Rees <maurits@vanrees.org>
2011-05-04 15:21:15 +0000

    pep8/pyflakes
    svn path=/smtp2zope/trunk/; revision=238617

--- smtp2zope/smtp2zope.py
+++ smtp2zope/smtp2zope.py
@@ -8,7 +8,7 @@
  URL      = call this URL with the email as a post-request
             Authentication can be included in URL:
             http://username:password@yourHost/...
-                
+
  MAXBYTES = optional: only forward mails < MAXBYTES to URL
 
  Please note: Output is logged to maillog per default on unices.
@@ -30,33 +30,45 @@
 """
 
 from stat import ST_NLINK, ST_MTIME
-import os, sys, socket, time, errno, random, tempfile, re
-import urllib, urllib2, base64
+import base64
+import errno
+import os
+import random
+import re
+import socket
+import sys
+import tempfile
+import time
+import urllib
+import urllib2
 
-##    
-# If you wish to use HTTP Basic Authentication, set a user id and password here.
-# Alternatively you can call the URL like:
+##
+# If you wish to use HTTP Basic Authentication, set a user id and
+# password here.  Alternatively you can call the URL like:
+#
 # http://username:password@yourHost/MailBoxer/manage_mailboxer
-# Note that this is not necessary in the default MailBoxer configuration, but
-# may be used to add some extra security.
+#
+# Note that this is not necessary in the default MailBoxer
+# configuration, but may be used to add some extra security.
+#
 # Format: username:password
-AUTHORIZATION=''
+AUTHORIZATION = ''
 
 ##
 # MAXBYTES is the number of maximum bytes per mail are allowed to
-# transfer to MailBoxer. 0 means unlimited. MAXBYTES can be
-# specified as last parameter when calling smtp2zope.py
+# transfer to MailBoxer. 0 means unlimited. MAXBYTES can be specified
+# as last parameter when calling smtp2zope.py
 MAXBYTES = 0
 
 ##
 # SPAM_TAGS are a list of regular expressions which will be checked
-# against the email before forwarding to Zope. 
-# If the regexp is found, the email will be discarded silently.
-SPAM_TAGS = ["\[SPAM\]","\[VIRUS\]"]
+# against the email before forwarding to Zope.  If the regexp is
+# found, the email will be discarded silently.
+SPAM_TAGS = ["\[SPAM\]", "\[VIRUS\]"]
 
 ##
-# If you have a special setup which don't allow locking / serialization,
-# set USE_LOCKS = 0
+# If you have a special setup which don't allow locking /
+# serialization, set USE_LOCKS = 0
 USE_LOCKS = 1
 
 ##
@@ -70,7 +82,7 @@ LOCK_TIMEOUT = 30
 
 ##
 # Number of seconds the process expects to hold the lock.
-DEFAULT_LOCK_LIFETIME  = 30
+DEFAULT_LOCK_LIFETIME = 30
 
 ##
 # Meaningful exit-codes for a smtp-server.
@@ -81,30 +93,34 @@ EXIT_TEMPFAIL = 75
 
 ##
 # REQUEST-paramter for submitted mail via URL
-MAIL_PARAMETER_NAME = "Mail"     
+MAIL_PARAMETER_NAME = "Mail"
 
 ##
 # Setup of loggers for error-messages
 try:
     import syslog
     syslog.openlog('mailboxer')
-    log_critical = lambda msg: syslog.syslog(syslog.LOG_CRIT|syslog.LOG_MAIL, msg)
-    log_error = lambda msg: syslog.syslog(syslog.LOG_ERR|syslog.LOG_MAIL, msg)
-    log_warning = lambda msg: syslog.syslog(syslog.LOG_WARNING|syslog.LOG_MAIL, msg)
-    log_info = lambda msg: syslog.syslog(syslog.LOG_INFO|syslog.LOG_MAIL, msg)
+    log_critical = lambda msg: syslog.syslog(
+        syslog.LOG_CRIT | syslog.LOG_MAIL, msg)
+    log_error = lambda msg: syslog.syslog(
+        syslog.LOG_ERR | syslog.LOG_MAIL, msg)
+    log_warning = lambda msg: syslog.syslog(
+        syslog.LOG_WARNING | syslog.LOG_MAIL, msg)
+    log_info = lambda msg: syslog.syslog(
+        syslog.LOG_INFO | syslog.LOG_MAIL, msg)
 except:
     # if we can't open syslog, just fake it
-    fake_logger = lambda msg: sys.stderr.write(msg+"\n")
+    fake_logger = lambda msg: sys.stderr.write(msg + "\n")
     log_critical = fake_logger
     log_error = fake_logger
     log_warning = fake_logger
     log_info = fake_logger
-           
+
 
 ##
 # Portable, NFS-safe file locking with timeouts.
 #
-# This code has taken from the GNU MailMan mailing list system, 
+# This code has taken from the GNU MailMan mailing list system,
 # with our thanks. Code was modified by Maik Jablonski.
 
 try:
@@ -114,17 +130,23 @@ except NameError:
     False = 0
 
 # Exceptions that can be raised by this module
+
+
 class LockError(Exception):
     """Base class for all exceptions in this module."""
+
 
 class AlreadyLockedError(LockError):
     """An attempt is made to lock an already locked object."""
 
+
 class NotLockedError(LockError):
     """An attempt is made to unlock an object that isn't locked."""
 
+
 class TimeOutError(LockError):
     """The timeout interval elapsed before the lock succeeded."""
+
 
 class LockFile:
     """A portable way to lock resources by way of the file system. """
@@ -145,7 +167,7 @@ class LockFile:
         # This works because we know we're single threaded
         self.__counter = LockFile.COUNTER
         LockFile.COUNTER += 1
-        self.__tmpfname = '%s.%s.%d.%d' % (lockfile, 
+        self.__tmpfname = '%s.%s.%d.%d' % (lockfile,
                                            socket.gethostname(),
                                            os.getpid(),
                                            self.__counter)
@@ -174,7 +196,7 @@ class LockFile:
             self.set_lifetime(newlifetime)
         # Do we have the lock?  As a side effect, this refreshes the lock!
         if not self.locked() and not unconditionally:
-            raise NotLockedError, '%s: %s' % (repr(self), self.__read())
+            raise NotLockedError('%s: %s' % (repr(self), self.__read()))
 
     def lock(self, timeout=0):
         """Acquire the lock.
@@ -217,12 +239,12 @@ class LockFile:
                     # happens, but for now we just say we didn't acquire the
                     # lock, and try again next time.
                     pass
-                elif e.errno <> errno.EEXIST:
+                elif e.errno != errno.EEXIST:
                     # Something very bizarre happened.  Clean up our state and
                     # pass the error on up.
                     os.unlink(self.__tmpfname)
                     raise
-                elif self.__linkcount() <> 2:
+                elif self.__linkcount() != 2:
                     # Somebody's messin' with us!
                     pass
                 elif self.__read() == self.__tmpfname:
@@ -260,12 +282,14 @@ class LockFile:
             try:
                 os.unlink(self.__lockfile)
             except OSError, e:
-                if e.errno <> errno.ENOENT: raise
+                if e.errno != errno.ENOENT:
+                    raise
         # Remove our tempfile
         try:
             os.unlink(self.__tmpfname)
         except OSError, e:
-            if e.errno <> errno.ENOENT: raise
+            if e.errno != errno.ENOENT:
+                raise
 
     def locked(self):
         """Return true if we own the lock, false if we do not.
@@ -284,7 +308,7 @@ class LockFile:
             else:
                 raise
         # TBD: can the link count ever be > 2?
-        if self.__linkcount() <> 2:
+        if self.__linkcount() != 2:
             return False
         return self.__read() == self.__tmpfname
 
@@ -315,7 +339,8 @@ class LockFile:
             fp.close()
             return filename
         except EnvironmentError, e:
-            if e.errno <> errno.ENOENT: raise
+            if e.errno != errno.ENOENT:
+                raise
             return None
 
     def __touch(self, filename=None):
@@ -324,41 +349,47 @@ class LockFile:
             # TBD: We probably don't need to modify atime, but this is easier.
             os.utime(filename or self.__tmpfname, (t, t))
         except OSError, e:
-            if e.errno <> errno.ENOENT: raise
+            if e.errno != errno.ENOENT:
+                raise
 
     def __releasetime(self):
         try:
             return os.stat(self.__lockfile)[ST_MTIME]
         except OSError, e:
-            if e.errno <> errno.ENOENT: raise
+            if e.errno != errno.ENOENT:
+                raise
             return -1
 
     def __linkcount(self):
         try:
             return os.stat(self.__lockfile)[ST_NLINK]
         except OSError, e:
-            if e.errno <> errno.ENOENT: raise
+            if e.errno != errno.ENOENT:
+                raise
             return -1
 
     def __break(self):
         try:
             self.__touch(self.__lockfile)
         except OSError, e:
-            if e.errno <> errno.EPERM: raise
+            if e.errno != errno.EPERM:
+                raise
         # Get the name of the old winner's temp file.
         winner = self.__read()
         # Remove the global lockfile, which actually breaks the lock.
         try:
             os.unlink(self.__lockfile)
         except OSError, e:
-            if e.errno <> errno.ENOENT: raise
+            if e.errno != errno.ENOENT:
+                raise
         # Try to remove the old winner's temp file, since we're assuming the
         # winner process has hung or died.
         try:
             if winner:
                 os.unlink(winner)
         except OSError, e:
-            if e.errno <> errno.ENOENT: raise
+            if e.errno != errno.ENOENT:
+                raise
 
     def __sleep(self):
         interval = random.random() * 2.0 + 0.01
@@ -376,7 +407,7 @@ if len(sys.argv) == 1 or len(sys.argv) > 3:
 # optional MAXBYTES?
 if len(sys.argv) == 3:
     try:
-        MAXBYTES  = long(sys.argv[2])
+        MAXBYTES = long(sys.argv[2])
     except ValueError:
         log_critical('Specified value of MAXBYTES (%s) was not an integer.'
                      % sys.argv[2])
@@ -384,7 +415,7 @@ if len(sys.argv) == 3:
 
 # Get the raw mail
 mailString = sys.stdin.read()
-            
+
 # Check size of mail
 mailLen = len(mailString)
 if MAXBYTES > 0 and mailLen > MAXBYTES:
@@ -406,7 +437,7 @@ if USE_LOCKS:
     except TimeOutError:
         log_info('Serialisation timeout occurred, message was requeued.')
         sys.exit(EXIT_TEMPFAIL)
-    
+
 # Transfer mail to http-server.
 # urllib2 handles server-responses (errors) much better than urllib.
 # urllib2 is, in fact, too much better. Its built-in redirect handler
@@ -415,6 +446,7 @@ if USE_LOCKS:
 # want to allow redirection of these requests!
 #
 # So, let's create and install a disfunctional redirect handler.
+
 
 class BrokenHTTPRedirectHandler(urllib2.HTTPRedirectHandler):
     def http_error_302(self, req, fp, code, msg, headers):
@@ -430,16 +462,17 @@ callURL = sys.argv[1]
 # Check for authentication-string (username:passwd) in URL
 # URL looks like: http://username:passwd@host/...
 auth_mark = callURL.find('@')
-if auth_mark<>-1:
-    AUTHORIZATION = callURL[:auth_mark].split('://')[1]    
-    callURL = callURL.replace(AUTHORIZATION+'@','')
+if auth_mark != -1:
+    AUTHORIZATION = callURL[:auth_mark].split('://')[1]
+    callURL = callURL.replace(AUTHORIZATION + '@', '')
 
 try:
     req = urllib2.Request(callURL)
     if AUTHORIZATION:
         auth = base64.encodestring(AUTHORIZATION).strip()
         req.add_header('Authorization', 'Basic %s' % auth)
-    urllib2.urlopen(req, data=MAIL_PARAMETER_NAME+"="+urllib.quote(mailString)) 
+    data = MAIL_PARAMETER_NAME + "=" + urllib.quote(mailString)
+    urllib2.urlopen(req, data=data)
 except Exception, e:
     # If MailBoxer doesn't exist, bounce message with EXIT_NOUSER,
     # so the sender will receive a "user-doesn't-exist"-mail from MTA.
@@ -448,7 +481,8 @@ except Exception, e:
         sys.exit(EXIT_NOUSER)
     else:
         # Server down? EXIT_TEMPFAIL causes the MTA to try again later.
-        log_error('A problem (%s) occurred uploading email to URL %s.' % (e, callURL))
+        log_error('A problem (%s) occurred uploading email to URL %s.' % (
+            e, callURL))
         sys.exit(EXIT_TEMPFAIL)
-        
+
 # All locks will be removed when Python cleans up!
